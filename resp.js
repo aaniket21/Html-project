3c99c68661bc811abd5b64b96664ec5f57faae15
Aniket <aniket.kr2103@gmail.com>
2023-11-26 01:58:53 +0530

    html viedo player added ,about section updated

--- resp.js
+++ resp.js
@@ -2,7 +2,16 @@ burger=document.querySelector('.burger')
 navbar=document.querySelector('.navbar')
 navList=document.querySelector('.nav-list')
 rightNav=document.querySelector('.rightNav')
+var viedoPlayer=document.getElementById("viedoPlayer");
+var myVideo=document.getElementById("myVideo");
 
+function stopVideo(){
+    viedoPlayer.style.display="none";
+}
+function playVideo(file){
+    myVideo.src=file;
+    viedoPlayer.style.display="block";
+}
 
 
 burger.addEventListener('click',()=>{
